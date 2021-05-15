c785b43f8787e79ed2f642501cea71f393fbe993
Israel Gadelha <gdelha.israel@gmail.com>
2021-05-15 18:12:44 -0300

    Banco de teste H2, perfil de teste e JPA

--- src/main/java/com/israelgda/webservice/entities/Usuario.java
+++ src/main/java/com/israelgda/webservice/entities/Usuario.java
@@ -2,10 +2,18 @@ package com.israelgda.webservice.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	//Atributos básicos do usuário
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
