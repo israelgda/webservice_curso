c2e5b97c0089adbf599ffc29a91dfd7c0ea8ad23
Israel Gadelha <gdelha.israel@gmail.com>
2021-05-24 19:53:58 -0300

    Produto-Pedido associação OnToMany

--- src/main/java/com/israelgda/webservice/entities/Produto.java
+++ src/main/java/com/israelgda/webservice/entities/Produto.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_produto")
@@ -31,6 +34,9 @@ public class Produto implements Serializable{
 	@ManyToMany
 	@JoinTable(name = "tb_produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))	
 	private Set<Categoria> categorias = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	//Construtores
 	
@@ -89,6 +95,15 @@ public class Produto implements Serializable{
 
 	public Set<Categoria> getCategorias() {
 		return categorias;
+	}
+	
+	@JsonIgnore
+	public Set<Pedido> getPedidos(){
+		Set<Pedido> pedidos = new HashSet<>();
+		for(ItemPedido ped: itens) {
+			pedidos.add(ped.getPedido());
+		}
+		return pedidos;
 	}
 	
 	//HashCode e Equals
