d22b88d7a83be27928cda405fc697eae08c5c0d3
Israel Gadelha <gdelha.israel@gmail.com>
2021-06-30 19:41:16 -0300

    Exception Handling - delete

--- src/main/java/com/israelgda/webservice/services/ServiceUsuario.java
+++ src/main/java/com/israelgda/webservice/services/ServiceUsuario.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.israelgda.webservice.entities.Usuario;
 import com.israelgda.webservice.repositories.RepositoryUsuarios;
+import com.israelgda.webservice.services.exceptions.DatabaseException;
 import com.israelgda.webservice.services.exceptions.ResourceNotFoundException;
 
 /*Anotação de Service serve para registrar como componente do Springboot, pelo uso do Anotation
@@ -37,7 +40,13 @@ public class ServiceUsuario {
 	
 	//Delete
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	//Update
